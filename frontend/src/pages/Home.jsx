f0cae22016a6cbc680004956512612de92f7d3b1
Vignesh J <125895606+vigneshvicky123456@users.noreply.github.com>
2024-01-10 13:59:39 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     useEffect(() => {
         setLoading(true);
         axios
-        .get('http://localhost:5555/books')
+        .get('https://mern-crud-vpg9.onrender.com/books')
         .then((response) => {
             setBooks(response.data.data);
             setLoading(false);
