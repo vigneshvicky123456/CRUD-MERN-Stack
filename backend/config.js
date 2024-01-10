8aab64eee589730146d352e904bedc620c21485c
Vignesh J <125895606+vigneshvicky123456@users.noreply.github.com>
2024-01-10 14:04:49 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = 'https://mern-crud-vpg9.onrender.com';
 
 export const mongoDBURL = 'mongodb+srv://vignesh:vignesh@cluster0.q3hwzgz.mongodb.net/books-collection?retryWrites=true&w=majority'
