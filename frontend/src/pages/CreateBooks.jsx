ab07e900dd8107965fa28b38158b1c60e7a0b3d7
Vignesh J <125895606+vigneshvicky123456@users.noreply.github.com>
2024-01-10 14:01:29 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-    .post('http://localhost:5555/books', data)
+    .post('https://mern-crud-vpg9.onrender.com/books', data)
     .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
